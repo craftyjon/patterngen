a96fdfcf7d16ef11195a45bbfe0027132fed8caa
Jon Evans <jon@craftyjon.com>
2012-03-27 21:49:50 -0400

    working pattern update dmeo

--- Demo.py
+++ Demo.py
@@ -2,29 +2,55 @@ import pygame
 import sys
 from pygame.locals import *
 import colorsys
+from threading import Timer
 
 from Preset import Preset
 #from CirclePreset import CirclePreset
 
-pygame.init()
+def on_tick():
+	global tick_timer, p
+	p.draw()
+	tick_timer = Timer(0.5, on_tick).start()
 
-size = width, height = 344, 320
 
-screen = pygame.display.set_mode(size)
+if __name__=="__main__":
+	pygame.init()
 
-s = pygame.Surface((24,24))
-sc = pygame.Surface((320,320))
-p = Preset((24,24))
+	size = width, height = 344, 320
 
-while 1:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+	screen = pygame.display.set_mode(size)
 
-	pygame.surfarray.blit_array(s, p.get_buffer())
+	#fps counter
+	last = 0
+	count = 0
+	now = pygame.time.get_ticks()
 
-	sc = pygame.transform.smoothscale(s, (320,320))
+	tick_timer = Timer(0.5, on_tick)
 
-	screen.blit(sc, sc.get_rect())
-	screen.blit(s, (320,0))
-	pygame.display.flip()
+	s = pygame.Surface((24,24))
+	sc = pygame.Surface((320,320))
+	p = Preset((24,24))
+
+	tick_timer.daemon = True
+	tick_timer.start()
+
+	while 1:
+		start_time = pygame.time.get_ticks()
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+		pygame.surfarray.blit_array(s, p.get_buffer())
+
+		sc = pygame.transform.smoothscale(s, (320,320))
+
+		screen.blit(sc, sc.get_rect())
+		screen.blit(s, (320,0))
+		pygame.display.flip()
+
+		count += 1
+		if pygame.time.get_ticks() - now > 1000.0:
+			now = pygame.time.get_ticks()
+			last = count
+			count = 0
+			print("fps:",last)
--- Preset.py
+++ Preset.py
@@ -1,19 +1,20 @@
 import numpy as np
-from Buffer import *
 
 class Preset:
 	def __init__(self, size=(24,24)):
 		self.buffer = np.zeros((size[0], size[1], 3), dtype=int)
 
 		for a in np.nditer(self.buffer, op_flags=['readwrite']):
-			if np.random.rand() > .75:
+			if np.random.rand() > .4:
 				a[...] = 200 * np.random.rand()
 
 	def setup(self):
 		pass
 
 	def draw(self):
-		pass
+		for a in np.nditer(self.buffer, op_flags=['readwrite']):
+			if np.random.rand() > .4:
+				a[...] = 200 * np.random.rand()
 
 	def get_buffer(self):
 		return self.buffer
